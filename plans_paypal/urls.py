a4b458a2b604d6addff356df4bdc900a1a0d45e9
Petr Dlouhý <petr.dlouhy@email.cz>
2020-01-08 22:19:34 +0100

    add django-paypal url to urls.py

--- plans_paypal/urls.py
+++ plans_paypal/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 
 urlpatterns = [
     path('paypal-payment/<int:order_id>/', views.view_that_asks_for_money, name='paypal-payment'),
+    path('paypal/', include('paypal.standard.ipn.urls')),
 ]
