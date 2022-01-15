19e677c3103c0baac3170c19eca1a476735515d3
Petr Dlouhý <petr.dlouhy@email.cz>
2022-01-15 10:25:35 +0100

    fix tests in Django 4.0

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import include, url
+from django.urls import include, path
 
 
 urlpatterns = [
-    url(r"^", include("plans_paypal.urls")),
+    path("", include("plans_paypal.urls")),
 ]
